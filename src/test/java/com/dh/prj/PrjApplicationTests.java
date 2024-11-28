93159267d1aa0881a36518fe283e49928b58d4fd
Dhananjay <dhananjayst0303@gmail.com>
2024-11-28 19:48:29 +0530

    first commit

---
+++ src/test/java/com/dh/prj/PrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
